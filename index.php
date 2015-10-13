0d9ad690d8ec4f819f9a651542b5326fbf41f83f
Bradley Brown <tall.white.ninja@gmail.com>
2015-10-13 10:58:22 -0600

    Added a list of my favorite hobbies and sports teams, and also a few blurbs in the "Goals" section

--- index.php
+++ index.php
@@ -9,8 +9,21 @@
 		<h2>Early Life</h2>
 		<p>Bradley was born in <a href="https://en.wikipedia.org/wiki/Albuquerque,_New_Mexico" target="_blank">Albuquerque, New Mexico</a> in 1987</p>
 		<h2>Hobbies and Interests</h2>
-		<p>fill in stuff</p>
+		<p>Bradley's hobbies and interests include:</p>
+		<ul>
+			<li>Video Games</li>
+			<li>Board Games</li>
+			<li>Warhammer 40k</li>
+		</ul>
+		<p>Bradley is also an avid sports watcher.  His favorite teams include:</p>
+		<ul>
+			<li>Everton FC</li>
+			<li>San Jose Earthquakes</li>
+			<li>San Francisco 49ers</li>
+			<li>University of New Mexico athletics</li>
+		</ul>
 		<h2>Future Goals</h2>
-		<p>fill in stuff</p>
+		<p>Bradley's current goal is to survive the Deep Dive Coding Bootcamp and become an established web developer.</p>
+		<p>Goals beyond that include taking a long soccer-watching tour of Europe and potentially conquering the known universe.</p>
 	</body>
 </html>
