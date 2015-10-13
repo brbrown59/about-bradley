886530e184d09ca96e419220832391f75379fbc9
Bradley Brown <tall.white.ninja@gmail.com>
2015-10-13 11:07:20 -0600

    Added links to sports teams

--- index.php
+++ index.php
@@ -7,20 +7,22 @@
 	<body>
 		<h1>About Bradley</h1>
 		<h2>Early Life</h2>
-		<p>Bradley was born in <a href="https://en.wikipedia.org/wiki/Albuquerque,_New_Mexico" target="_blank">Albuquerque, New Mexico</a> in 1987</p>
+		<p>Bradley was born in <a href="https://en.wikipedia.org/wiki/Albuquerque,_New_Mexico" target="_blank">Albuquerque, New Mexico</a> in 1987.
+		Other than brief childhood years spent in El Paso, TX and Gallup, NM, he has lived in the Albuquerque area his entire life.</p>
 		<h2>Hobbies and Interests</h2>
 		<p>Bradley's hobbies and interests include:</p>
 		<ul>
 			<li>Video Games</li>
 			<li>Board Games</li>
 			<li>Warhammer 40k</li>
+			<li>Magic: the Gathering (retired)</li>
 		</ul>
 		<p>Bradley is also an avid sports watcher.  His favorite teams include:</p>
 		<ul>
-			<li>Everton FC</li>
-			<li>San Jose Earthquakes</li>
-			<li>San Francisco 49ers</li>
-			<li>University of New Mexico athletics</li>
+			<li><a href="http://www.evertonfc.com/" target="_blank">Everton FC</a></li>
+			<li><a href="http://www.sjearthquakes.com/" target="_blank">San Jose Earthquakes</a></li>
+			<li><a href="http://www.49ers.com/" target="_blank">San Francisco 49ers</a></li>
+			<li><a href="http://www.golobos.com/" target="_blank">University of New Mexico athletics</a></li>
 		</ul>
 		<h2>Future Goals</h2>
 		<p>Bradley's current goal is to survive the Deep Dive Coding Bootcamp and become an established web developer.</p>
