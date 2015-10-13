3c38824ad90263ad4f97a234e420007eec2440f9
Bradley Brown <tall.white.ninja@gmail.com>
2015-10-13 10:45:32 -0600

    Added the headers for each topic about me.  Also added the first sentence of the "early life" section and added an external link to wikipedia

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <!DOCTYPE html>
-	<html>
+	<html lang="en">
 	<head>
-		<title>About me</title>
+		<meta charset="utf-8"/>
+		<title>About Bradley</title>
 	</head>
 	<body>
-		<p>This is about me</p>
+		<h1>About Bradley</h1>
+		<h2>Early Life</h2>
+		<p>Bradley was born in <a href="https://en.wikipedia.org/wiki/Albuquerque,_New_Mexico" target="_blank">Albuquerque, New Mexico</a> in 1987</p>
+		<h2>Hobbies and Interests</h2>
+		<p>fill in stuff</p>
+		<h2>Future Goals</h2>
+		<p>fill in stuff</p>
 	</body>
 </html>
